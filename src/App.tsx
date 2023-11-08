c07265326f261df31961cd4ed6999bfbc32f4b1d
Mark Khramov <hramov.mark321@gmail.com>
2023-11-08 12:35:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/scenes/navbar/Navbar.tsx";
+
+function App() {
+    return (
+        <div className='app bg-gray-20'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
---
+++ src/scenes/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png"
+
+type Props = {}
+const Navbar = (props: Props) => {
+    const flexBetween = "flex items-center justify-between"
+    return <nav>
+        <div
+            className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
+        >
+            <div className={`${flexBetween} mx-auto w-5/6`}>
+                <div className={`${flexBetween} w-full gap-16`}>
+                    {/* LEFT SIDE */}
+                    <img alt='logo' src={Logo}/>
+
+                    {/* RIGHT SIDE */}
+                    <div className={`${flexBetween} w-full`}>
+                      <div className={`${flexBetween} gap-8 text-sm`}>
+
+                      </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </nav>
+        ;
+};
+export default Navbar;
