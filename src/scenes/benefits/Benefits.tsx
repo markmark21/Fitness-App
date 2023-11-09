7eb0371773edb5bc3e7da76df6f4af858b33265c
Mark Khramov <hramov.mark321@gmail.com>
2023-11-09 14:23:53 +0300

    Animations

--- src/scenes/benefits/Benefits.tsx
+++ src/scenes/benefits/Benefits.tsx
@@ -42,14 +42,24 @@ const Benefits = ({setSelectedPage}: Props) => {
             onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
         >
             {/* HEADER */}
-            <div className='md:my-5 md:w-3/5'>
+            <motion.div
+                className='md:my-5 md:w-3/5'
+                initial='hidden'
+                whileInView='visible'
+                viewport={{once: true, amount: 0.5}}
+                transition={{delay: 0.2, duration: 0.5}}
+                variants={{
+                    hidden: {opacity: 0, x: -300},
+                    visible: {opacity: 1, x: 0}
+                }}
+            >
                 <Htext>MORE THAN JUST A GYM.</Htext>
                 <p className='my-5 text-sm'>
                     We provide world class fitness equipment, trainers and classes to
                     get you to your ultimate fitness goals with ease. We provide true
                     care into each and every member.
                 </p>
-            </div>
+            </motion.div>
             {/* BENEFITS */}
             <motion.div
                 className='md:flex items-center justify-between gap-8 mt-5'
