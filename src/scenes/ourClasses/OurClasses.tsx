b6b3f387cffcc876fe1490ca64ef4b4ee9ae95df
Mark Khramov <hramov.mark321@gmail.com>
2023-11-09 16:54:50 +0300

    OurClasses

--- src/scenes/ourClasses/OurClasses.tsx
+++ src/scenes/ourClasses/OurClasses.tsx
@@ -18,7 +18,6 @@ const classes: Array<ClassType> = [
     },
     {
         name: 'Yoga Classes',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         image: image2,
     },
     {
@@ -33,7 +32,6 @@ const classes: Array<ClassType> = [
     },
     {
         name: 'Fitness Classes',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         image: image5,
     },
     {
